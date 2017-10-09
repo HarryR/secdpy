f9292bc80c66283e0715b97061b124862a9b40b5
user.name <user@email>
2017-10-09 22:48:25 +0100

    Re-implemented secd virtual machine

--- repl.py
+++ repl.py
@@ -1,11 +1,23 @@
+from __future__ import print_function
 from codegen import *
 from secd    import *
 import sys
 
 # read eval print loop
 
-machine = SECD()
 while True:
-    line = input('SECDrepl> ')
-    code = codegen(parse(line), [], ['stop'])
-    machine.runProgram(code, [])
+	try:
+		line = raw_input('> ')
+	except EOFError:
+		break
+	print("Line:", line)
+
+	parsed = parse(line)
+	print("Parsed:", parsed)
+
+	code = codegen(parsed, [], ['stop'])
+	print("Code:", code)
+
+	result = secd_eval(code).s.pop()
+	print("=", result)
+
--- secd.py
+++ secd.py
@@ -1,187 +1,88 @@
-import sys, ast
+import sys
+import ast
+import operator
+from collections import namedtuple 
 
-class SECD:
+State = namedtuple('State', ('s', 'e', 'c', 'd'))
 
-    def __init__(self):
-        self.s = []
-        self.e = []
-        self.c = []
-        self.d = []
-        self.running = True
+APPLY = lambda S, N, F: F(*[x.pop() for x in [S.s] * N])
+PEEK = lambda S, N, F: F(*S.s[-1:0-N])
 
-    def runProgram(self,code,stack=[]):
-        self.running = True
-        self.loadProgram(code,stack)
-        self.run()
+COMMANDS = {
+	'ADD': (APPLY, 2, operator.add),
+	'MUL': (APPLY, 2, operator.mul),
+	'SUB': (APPLY, 2, operator.sub),
+	'DIV': (APPLY, 2, operator.div),
+	'XOR': (APPLY, 2, operator.xor),
+	'EQ': (PEEK, 2, operator.eq),
+	'LT': (PEEK, 2, operator.lt),
+	'GT': (PEEK, 2, operator.gt),
+	'LEQ': (PEEK, 2, operator.le),
+	'GEQ': (PEEK, 2, operator.ge),
+	'CONS': (APPLY, 2, lambda car, cdr: [cdr.append(car), cdr][1]),
+	'CAR': (APPLY, 1, lambda x: x.pop()),
+	'CDR': (APPLY, 1, lambda x: [x.pop(), x][1]),
+	'NIL': (lambda _: list(),),
+	'ATOM': (PEEK, 1, lambda x: x == []),
+	'LDC': (lambda S: S.c.pop(),),  # pushes constant argument onto stack
+	'LDF': (lambda (s, e, c, d): [e[:], c.pop()],)
+}
 
-    def loadProgram(self, code, stack=[]):
-        self.s = stack
-        self.e = []
-        self.c = code
-        self.d = []
+def secd_eval(code, stack=[]):
+	state, running = State(stack, [], code, []), True
+	while running:
+		state, running = secd_step(state)
+	return state
 
-    def readout(self):
-        print('S: ', self.s)
-        print('E: ', self.e)
-        print('C: ', self.c)
-        print('D: ', self.d)
-        print()
+def secd_step(state):
+	s, e, c, d = state
+	stop = False
+	op = c.pop().upper()
+	if op in COMMANDS:
+		cmd = COMMANDS[op]
+		retval = cmd[0](*[state] + list(cmd[1:]))
+		if retval is not None:
+			s.append(retval)
+	elif op == 'STOP':
+		stop = True
+	elif op == 'AP':
+		d.extend([c, e, s])
+		ce = s.pop()
+		c, e = ce.pop(), ce.pop()
+		e.append(s.pop())
+		s = []
+	elif op == 'LD':  # pushes the value of a variable onto the stack
+		x, y = c.pop()
+		s.append(e[-x][-y])
+	elif op == 'DUM':
+		e.append([])
+	elif op == 'RAP':
+		closure = s.pop()
+		fn, env = closure.pop(), closure.pop()
+		env.pop()
+		v = s.pop()
+		e.pop()
+		d.extend([c, e, s])
+		e = env
+		e.append(v)
+		c = fn
+		s = []
+	elif op == 'JOIN':
+		c = [d.pop()]
+	elif op == 'RTN':
+		retval = s.pop()
+		s = d.pop()
+		s.append(retval)
+		e, c = d.pop(), d.pop()
+	elif op == 'SEL':
+		x = s.pop()
+		ct, cf = c.pop(), c.pop()
+		d.append(c)
+		c = [ct if x else cf]
 
-    def run(self):
-        self.readout()
-        while self.running:
-            self.execute()
-            self.readout()
-
-    def execute(self):
-        cmd = self.c.pop().upper()
-        op = {'ADD':   self.perform_add,
-              'MUL':   self.perform_mul,
-              'SUB':   self.perform_sub,
-              'DIV':   self.perform_div,
-              'CAR':   self.perform_car,
-              'CDR':   self.perform_cdr,
-              'NIL':   self.perform_nil,
-              'ATOM':  self.perform_atom,
-              'CONS':  self.perform_cons,
-              'LDC':   self.perform_ldc,
-              'LDF':   self.perform_ldf,
-              'AP':    self.perform_ap,
-              'LD':    self.perform_ld,
-              'DUM':   self.perform_dum,
-              'RAP':   self.perform_rap,
-              'JOIN':  self.perform_join,
-              'RTN':   self.perform_rtn,
-              'SEL':   self.perform_sel,
-              'ZERO':  self.perform_zero,
-              'EQ':    self.perform_eq,
-              'LEQ':   self.perform_leq,
-              'STOP':  self.perform_stop,
-            }[cmd]
-
-        op()
-
-    def perform_stop(self):
-        self.running = False
-
-    def perform_add(self):
-        a = self.s.pop()
-        b = self.s.pop()
-        self.s.append(a + b)
-
-    def perform_sub(self):
-        a = self.s.pop()
-        b = self.s.pop()
-        self.s.append(a - b)
-
-    def perform_mul(self):
-        a = self.s.pop()
-        b = self.s.pop()
-        self.s.append(a * b)
-
-    def perform_div(self):
-        a = self.s.pop()
-        b = self.s.pop()
-        self.s.append(a / b)
-
-    def perform_nil(self):
-        self.s.append([])
-
-    def perform_ldc(self):
-        a = self.c.pop()
-        self.s.append(a)
-
-    def perform_ldf(self):
-        fn  = self.c.pop()
-        env = self.e[:]
-        self.s.append([env,fn])
-
-    def perform_ap(self):
-        self.d.append(self.c)
-        self.d.append(self.e)
-        self.d.append(self.s)
-        ce = self.s.pop()
-        self.c = ce.pop()
-        self.e = ce.pop()
-        self.e.append(self.s.pop())
-        self.s = []
-
-    def perform_rtn(self):
-        retval = self.s.pop()
-        self.s = self.d.pop()
-        self.s.append(retval)
-        self.e = self.d.pop()
-        self.c = self.d.pop()
-
-    def perform_join(self):
-        self.c = []
-        self.c.append(self.d.pop())
-
-    def perform_sel(self):
-        x = self.s.pop()
-        ct = self.c.pop()
-        cf = self.c.pop()
-        self.d.append(self.c)
-        self.c = []
-        if x:
-            self.c.append(ct)
-        else:
-            self.c.append(cf)
-
-    def perform_ld(self):
-        loc = self.c.pop()
-        x,y = loc.pop(),loc.pop()
-        val = self.e[-x][-y] # index from end
-        self.s.append(val)
-
-    def perform_dum(self):
-        """ create a dummy env """
-        self.e.append([])
-
-    def perform_rap(self):
-        closure = self.s.pop()
-        fn = closure.pop()
-        env = closure.pop()
-        env.pop()
-        v = self.s.pop()
-        self.e.pop()
-        self.d.append(self.c)
-        self.d.append(self.e)
-        self.d.append(self.s)
-        self.e = env
-        self.e.append(v)
-        self.c = fn
-        self.s = []
-
-    def perform_atom(self):
-        self.s.append(self.s[-1] == [])
-
-    def perform_cons(self):
-        car = self.s.pop()
-        cdr = self.s.pop()
-        cdr.append(car)
-        self.s.append(cdr)
-
-    def perform_car(self):
-        xs = self.s.pop()
-        car = xs.pop()
-        self.s.append(car)
-
-    def perform_cdr(self):
-        xs = self.s.pop()
-        xs.pop()
-        self.s.append(xs)
-
-    def perform_zero(self):
-        self.s.append(self.s[-1] == 0)
-
-    def perform_eq(self):
-        self.s.append(self.s[-1] == self.s[-2])
-
-    def perform_leq(self):
-        self.s.append(self.s[-1] <= self.s[-2])
+	return State(s, e, c, d), not(stop)
 
 if __name__ == "__main__":
     code = ast.literal_eval(sys.stdin.read())
-    machine = SECD()
-    machine.runProgram(code)
+    result = secd_eval(code)
+
